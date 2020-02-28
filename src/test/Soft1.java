93f96a9d35672b8ff19ea1b64de2378618f37a1d
mohammadmuslh <53003255+mohammadmuslh@users.noreply.github.com>
2020-02-28 15:39:22 +0200

    Update Soft1.java

--- src/test/Soft1.java
+++ src/test/Soft1.java
@@ -1,11 +1,10 @@
 package test;
 
 import soft.*;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
  
 class Soft1 {
-	@Test
+	]
 	void test() {
 		Soft o = new Soft();
 	int r =	o.add(100,200);
